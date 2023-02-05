97c252c2bd59da4b5fbc323662b8c46e684ed591
Vijisha15 <124337944+Vijisha15@users.noreply.github.com>
2023-02-05 19:18:57 +0530

    Add files via upload

---
+++ Flow Builder.js
@@ -0,0 +1,116 @@
+// index.tsx
+import React, { useState, useContext } from 'react';
+import FlowBuilder, {
+  NodeContext,
+  INode,
+  IRegisterNode,
+} from 'flowbuilder';
+
+import './index.css';
+
+const StartNodeDisplay: React.FC = () => {
+  const node = useContext(NodeContext);
+  return <div className="start-node">{node.name}</div>;
+};
+
+const EndNodeDisplay: React.FC = () => {
+  const node = useContext(NodeContext);
+  return <div className="end-node">{node.name}</div>;
+};
+
+const OtherNodeDisplay: React.FC = () => {
+  const node = useContext(NodeContext);
+  return <div className="other-node">{node.name}</div>;
+};
+
+const ConditionNodeDisplay: React.FC = () => {
+  const node = useContext(NodeContext);
+  return <div className="condition-node">{node.name}</div>;
+};
+
+const registerNodes: IRegisterNode[] = [
+  {
+    type: 'start',
+    name: 'start node',
+    displayComponent: StartNodeDisplay,
+    isStart: true,
+  },
+  {
+    type: 'end',
+    name: 'end node',
+    displayComponent: EndNodeDisplay,
+    isEnd: true,
+  },
+  {
+    type: 'node',
+    name: 'other node',
+    displayComponent: OtherNodeDisplay,
+  },
+  {
+    type: 'condition',
+    name: 'condition node',
+    displayComponent: ConditionNodeDisplay,
+  },
+  {
+    type: 'branch',
+    name: 'branch node',
+    conditionNodeType: 'condition',
+  };
+];
+
+const Demo = () => {
+  const [nodes, setNodes] = useState<INode[]>([]);
+
+  const handleChange = (nodes: INode[]) => {
+    console.log('nodes change', nodes);
+    setNodes(nodes);
+  };
+
+  return (
+    <FlowBuilder
+      nodes={nodes}
+      onChange={handleChange}
+      registerNodes={registerNodes}
+    />
+  );
+};
+
+export default Demo;
+
+// index.css
+.start-node, .end-node {
+  height: 64px;
+  width: 64px;
+  border-radius: 50%;
+  line-height: 64px;
+  color: #fff;
+  text-align: center;
+}
+
+.start-node {
+  background-color: #338aff;
+}
+
+.end-node {
+  background-color: #666;
+}
+
+.other-node, .condition-node {
+  width: 224px;
+  border-radius: 4px;
+  color: #666;
+  background: #fff;
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.08);
+}
+
+.other-node {
+  height: 118px;
+  padding: 16px;
+  display: flex;
+  flex-direction: column;
+}
+
+.condition-node {
+  height: 44px;
+  padding: 12px 16px;
+}
